9ff6f36697fc08ad83dcfe84d0eda47b4872ad8c
musin <betwelmusin@gmail.com>
2022-10-14 14:53:32 +0300

    deployment

--- studybud/settings.py
+++ studybud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developerchatroom.herokuapp.com', ]
 
 
 # Application definition
